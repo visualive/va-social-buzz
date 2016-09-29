765d854a774fc57ad7da3d8f30f16fdb485e9ec6
KUCKLU <kuck1u@visualive.jp>
2016-09-30 02:50:09 +0900

    get_thumbnailメソッドで、postオブジェクトが空じゃないことを確認するようにした。 get_thumbnailメソッドの返り値の初期値をnoneに変更。

--- incs/trait-functions.php
+++ incs/trait-functions.php
@@ -59,19 +59,21 @@ namespace VASOCIALBUZZ\Modules {
 		 * @return string
 		 */
 		public static function get_thumbnail( $_post = null ) {
-			$thumb = '';
+			$thumb = 'none';
 
 			if ( empty( $_post ) ) {
 				global $post;
 				$_post = $post;
 			}
 
-			if ( has_post_thumbnail( $_post ) && ! post_password_required( $_post ) ) {
-				$thumb = get_the_post_thumbnail_url( $_post, VA_SOCIALBUZZ_PREFIX . 'thumbnail' );
-			} elseif ( has_header_image() ) {
-				$thumb = get_header_image();
-			} elseif ( has_site_icon() ) {
-				$thumb = get_site_icon_url();
+			if ( ! empty( $_post ) ) {
+				if ( has_post_thumbnail( $_post ) && ! post_password_required( $_post ) ) {
+					$thumb = get_the_post_thumbnail_url( $_post, VA_SOCIALBUZZ_PREFIX . 'thumbnail' );
+				} elseif ( has_header_image() ) {
+					$thumb = get_header_image();
+				} elseif ( has_site_icon() ) {
+					$thumb = get_site_icon_url();
+				}
 			}
 
 			return $thumb;
