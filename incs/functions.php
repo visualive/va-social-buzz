02ca766881e209befff8c09dfe7f5966225ea08a
KUCKLU <kuck1u@visualive.jp>
2016-10-02 03:52:52 +0900

    言語ファイルの読み込みを強制的にwp-content/plugins/va-social-buzz/lang内へ変更。

--- incs/functions.php
+++ incs/functions.php
@@ -26,6 +26,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+add_filter( 'load_textdomain_mofile', function ( $mofile, $domain ) {
+	if ( 'en' !== get_locale() && VA_SOCIALBUZZ_BASENAME === $domain ) {
+		$mofile = sprintf( '%s/langs/%s-%s.mo', VA_SOCIALBUZZ_PATH, VA_SOCIALBUZZ_BASENAME, get_locale() );
+	}
+
+	return $mofile;
+}, 10, 2 );
 load_plugin_textdomain( 'va-social-buzz', false, VA_SOCIALBUZZ_BASENAME . '/langs' );
 
 /**
